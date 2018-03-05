761d50c0771f3d8ae847e571f62d89a94e9f8159
Oleksii Teplenko <pytnik.tov@gmail.com>
2018-03-05 23:33:22 +0200

    add loging, error handling, automatic reloading

---
+++ api/routes/products.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products',
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'Congrats! This is the special ID!!',
+            id,
+        });
+    } else {
+        res.status(200).json({
+            message: 'This is some boring ID',
+        });
+    }
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'Congrats! This is the special ID!!',
+            id,
+        });
+    } else {
+        res.status(200).json({
+            message: 'This is some boring ID',
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product',
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product',
+    });
+});
+
+module.exports = router;
